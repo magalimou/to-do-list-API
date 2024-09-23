86a455a5f44f29a5e9c37902aa121d27e1cdca8e
magali <maguimourino@gmail.com>
2024-09-22 21:11:24 -0300

    database connection

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const db = require('./src/db');
 
 dotenv.config();
 
@@ -9,6 +10,16 @@ const port = process.env.PORT || 5000;
 app.use(express.json()); // Para manejar JSON en las solicitudes
 
 // Rutas
+app.get('/db-test', async (req, res) => {
+    try {
+      const [rows] = await db.query('SELECT 1 + 1 AS solution');
+      res.json({ message: `La solución es: ${rows[0].solution}` });
+    } catch (error) {
+      console.error('Error en la conexión a la base de datos:', error);
+      res.status(500).json({ error: 'Error en la conexión a la base de datos' });
+    }
+});
+
 app.get('/', (req, res) => {
   res.send('API funcionando');
 });
